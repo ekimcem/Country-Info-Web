6ae936384b1fcacfed217fd4a2ea60c8506079b0
Ekim Cem Ülger <ekimcemulger@gmail.com>
2022-01-04 23:49:11 +0300

    empty array from API resolved

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let countries; // fetched data will be storage here
 
 const cardContainer = document.querySelector("#card-container");
+
 fetch("https://restcountries.com/v3.1/all")
   .then((res) => res.json())
   .then((data) => initialize(data))
@@ -8,12 +9,12 @@ fetch("https://restcountries.com/v3.1/all")
 
 function initialize(data) {
   countries = data;
-  
-  
+
   let cards = "";
-  
+
   countries.forEach((country) => {
-    console.log(Object.keys(country.currencies));
+    console.log(country);
+
     cards += `<div class="card m-3" style="width: 250px;">
     <img src="${country.flags.png}" class="card-img-top">
     <div class="card-body">
@@ -21,12 +22,13 @@ function initialize(data) {
       <ul class="list-group list-group-flush">
       <li class="list-group-item">Capital City: ${country.capital}</li>
       <li class="list-group-item">Population: ${country.population.toLocaleString()}</li>
-      <li class="list-group-item">Currencies: ${Object.keys(country.currencies)}</li>
-    
+      <li class="list-group-item">Currencies: ${
+        country.currencies ? Object.keys(country.currencies) : "-"
+      }</li>
     </ul>
     </div>
   </div>`;
-  
+
     cardContainer.innerHTML = cards;
   });
 }
